22ec2e8ead0563ffdb6be7c5c679992b081f6875
Archa S Mohan <archa.mohan@experionglobal.local>
2024-01-23 15:01:15 +0530

    fix-documentation

---
+++ Sample/src/com/ilp/services/UserValidation.java
@@ -0,0 +1,30 @@
+package com.ilp.services;
+
+public class UserValidation {
+	
+  public void validate(String name,String email,String password) 
+     {
+	  //logic
+       }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class UserAuthentication {
+	
+	 public void authenticate(String name,String email,String password) {
+	
+}}
---
+++ Sample/src/com/ilp/services/Validation.java
@@ -0,0 +1,18 @@
+package com.ilp.services;
+
+public class Validation {
+	public void Emailvalidate(String email) 
+    {
+	  //logic
+      }
+	public void Namevalidate(String name) 
+    {
+	  //logic
+      }
+	public void Passwordvalidate(String password) 
+    {
+	  //logic
+      }
+ 
+}
+
